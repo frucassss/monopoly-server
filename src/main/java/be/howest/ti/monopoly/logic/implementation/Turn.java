6deb9a1665d9260042501df098a19310c9583638
Michiel Stragier <Michiel.stragier@gmail.com>
2022-05-02 18:26:49 +0200

    roll dice function

--- src/main/java/be/howest/ti/monopoly/logic/implementation/Turn.java
+++ src/main/java/be/howest/ti/monopoly/logic/implementation/Turn.java
@@ -7,7 +7,6 @@ public class Turn {
     private int[] roll = new int[2];
     private Player player= new Player;
     private List<Move> moves = new ArrayList<>();
-
     Turn(int[] roll, Player player, List<Move> moves){
         this.moves = moves;
         this.player = player;
@@ -24,5 +23,12 @@ public class Turn {
 
     public List<Move> getMoves() {
         return moves;
+    }
+
+    public void rollDice(){
+        int dice1 = (int) ((Math.random()*(6-1)) + 1);
+        int dice2 = (int) ((Math.random()*(6-1)) + 1);
+        roll[0] = dice1;
+        roll[1] = dice2;
     }
 }
