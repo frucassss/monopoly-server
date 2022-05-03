a295711e6d1ff35367652b20a97e5c09c4580c94
Michiel Stragier <Michiel.stragier@gmail.com>
2022-05-03 09:58:00 +0200

    add todo

--- src/main/java/be/howest/ti/monopoly/logic/implementation/Turn.java
+++ src/main/java/be/howest/ti/monopoly/logic/implementation/Turn.java
@@ -1,34 +1,30 @@
 package be.howest.ti.monopoly.logic.implementation;
 
+import be.howest.ti.monopoly.logic.implementation.tile.Tile;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Turn {
     private int[] roll = new int[2];
-    private Player player= new Player;
+    private Player player;
     private List<Move> moves = new ArrayList<>();
-    Turn(int[] roll, Player player, List<Move> moves){
-        this.moves = moves;
+
+    Turn(Player player){
         this.player = player;
-        this.roll = roll;
     }
 
     public int[] getRoll() {
         return roll;
     }
 
-    public Player getPlayer() {
-        return player.getName;
+    public String getPlayer() {
+        return player.getName();
     }
 
-    public List<Move> getMoves() {
-        return moves;
-    }
+    public List<Move> getMoves() { return moves; }
 
-    public void rollDice(){
-        int dice1 = (int) ((Math.random()*(6-1)) + 1);
-        int dice2 = (int) ((Math.random()*(6-1)) + 1);
-        roll[0] = dice1;
-        roll[1] = dice2;
+    public void addMove(Tile tile){
+        //todo je moet controleren of de tile al van iemand is of niet, is dus misschien verstandigst om eerst de game classe te hebben.
     }
 }
