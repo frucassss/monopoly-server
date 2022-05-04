7b8a7554a9bb412f849a9f26ef1979918bcce977
Michiel Stragier <Michiel.stragier@gmail.com>
2022-05-04 13:44:46 +0200

    changed move to get chance and community chest vie parameter

--- src/main/java/be/howest/ti/monopoly/logic/implementation/Move.java
+++ src/main/java/be/howest/ti/monopoly/logic/implementation/Move.java
@@ -1,16 +1,24 @@
 package be.howest.ti.monopoly.logic.implementation;
 
+import be.howest.ti.monopoly.logic.exceptions.IllegalMonopolyActionException;
 import be.howest.ti.monopoly.logic.implementation.tile.Tile;
 
+import java.util.List;
 import java.util.Objects;
 import java.util.Random;
 
 public class Move {
     private Tile tile;
     private String description;
+    protected List<Tile> tiles;
+    protected List<String> chance;
+    protected List<String> communityChest;
 
-    Move(Tile tile){
+    Move(Tile tile, List<String> chance, List<String> communityChest,List<Tile> tiles){
         this.tile = tile;
+        this.tiles = tiles;
+        this.chance = chance;
+        this.communityChest = communityChest;
         switch (tile.getType()){
             case "chance":
                 description = getRandomChance();
@@ -41,21 +49,21 @@ public class Move {
                 description = "has to go to jail";
                 break;
             default:
-                break;
+                throw new IllegalMonopolyActionException("This tile type doesn't exist?");
         }
 
     }
 
     private String getRandomChance(){
         Random random = new Random();
-        int value = random.nextInt(15 + 1) + 1;
-        return Monopoly.getChance().get(value);
+        int value = random.nextInt(chance.size() - 1) + 1;
+        return chance.get(value);
     }
 
     private String getRandomCommunityChest(){
         Random random = new Random();
-        int value = random.nextInt(16 + 1) + 1;
-        return Monopoly.getCommunityChest().get(value);
+        int value = random.nextInt(communityChest.size() - 1) + 1;
+        return communityChest.get(value);
     }
 
     public String getTile() {
