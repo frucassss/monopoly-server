cea387e77aa65dd65a0c7fcefc470e63223d6817
Lucas Guillemyn <lucas.guillemyn@student.howest.be>
2022-05-09 18:28:46 +0200

    logic done

--- src/main/java/be/howest/ti/monopoly/web/MonopolyApiBridge.java
+++ src/main/java/be/howest/ti/monopoly/web/MonopolyApiBridge.java
@@ -251,11 +251,10 @@ public class MonopolyApiBridge {
         Request request = Request.from(ctx);
         String gameId = request.getGameIdFromPath();
         String playerName = request.getPlayerNameFromPath();
-        String authorization = ctx.request().headers().get(HttpHeaderNames.AUTHORIZATION);
 
-        /*if(!request.isAuthorized(gameId, playerName)){
+        if(!request.isAuthorized(gameId, playerName)){
             throw new ForbiddenAccessException("This is a protected endpoint. Make sure the security-token you passed along is valid token for this game.");
-        }*/
+        }
 
         service.declareBankruptcy(gameId, playerName);
 
