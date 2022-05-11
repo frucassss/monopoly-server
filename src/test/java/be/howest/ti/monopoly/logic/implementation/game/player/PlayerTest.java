202ba6296b03e2d0cb2ef279bd38217855f5478b
Michiel Stragier <Michiel.stragier@gmail.com>
2022-05-11 19:31:15 +0200

    add test to player (just to have a test there xD)

--- src/test/java/be/howest/ti/monopoly/logic/implementation/game/player/PlayerTest.java
+++ src/test/java/be/howest/ti/monopoly/logic/implementation/game/player/PlayerTest.java
@@ -16,6 +16,20 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerTest {
+    private final Player michiel = new Player("michiel", new Game("dummy", 1, 2, new ArrayList<>(), new ArrayList<>(), new ArrayList<>()));
 
+    @Test
+    void testPayingNegativeNumber() {
+        assertThrows(IllegalMonopolyActionException.class, () -> {
+            michiel.pay(-10);
+        });
+    }
+
+    @Test
+    void testCollectingNegativeNumber(){
+        assertThrows(IllegalMonopolyActionException.class,()->{
+            michiel.collect(-10);
+        });
+    }
 
 }
