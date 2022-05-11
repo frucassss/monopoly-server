00308d0194ed3524f9cc5cd827cafd3f0663263b
Michiel Stragier <Michiel.stragier@gmail.com>
2022-05-11 22:37:35 +0200

    refactor code

--- src/test/java/be/howest/ti/monopoly/logic/implementation/game/player/PlayerTest.java
+++ src/test/java/be/howest/ti/monopoly/logic/implementation/game/player/PlayerTest.java
@@ -1,13 +1,8 @@
 package be.howest.ti.monopoly.logic.implementation.game.player;
 
 import be.howest.ti.monopoly.logic.exceptions.IllegalMonopolyActionException;
-import be.howest.ti.monopoly.logic.implementation.MonopolyService;
 import be.howest.ti.monopoly.logic.implementation.game.Game;
-import be.howest.ti.monopoly.logic.implementation.game.player.property.Improve;
-import be.howest.ti.monopoly.logic.implementation.game.player.property.Mortgage;
-import be.howest.ti.monopoly.logic.implementation.game.player.property.Property;
-import be.howest.ti.monopoly.logic.implementation.tile.StreetTile;
-import be.howest.ti.monopoly.logic.implementation.tile.Tile;
+
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
