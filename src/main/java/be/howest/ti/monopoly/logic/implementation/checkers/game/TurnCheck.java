4e3a50e0b5c3eecbad6844055e518c2a902c4348
Lucas Guillemyn <lucas.guillemyn@student.howest.be>
2022-05-12 12:20:28 +0200

    added if player should be bankrupt when balance is below 0

--- src/main/java/be/howest/ti/monopoly/logic/implementation/checkers/game/TurnCheck.java
+++ src/main/java/be/howest/ti/monopoly/logic/implementation/checkers/game/TurnCheck.java
@@ -1,4 +1,31 @@
 package be.howest.ti.monopoly.logic.implementation.checkers.game;
 
+import be.howest.ti.monopoly.logic.implementation.game.Game;
+import be.howest.ti.monopoly.logic.implementation.game.Turn;
+import be.howest.ti.monopoly.logic.implementation.game.player.Player;
+
 public class TurnCheck {
+
+    private final Turn turn;
+    /*private final Game game;
+    private final Player player;*/
+
+    public TurnCheck(Turn turn){
+        this.turn = turn;
+        /*this.game = turn.receiveGame();
+        this.player = turn.receivePlayer();*/
+    }
+
+    public void checkIfPlayerShouldBeBankrupt(String playerName){
+        Player p = game.findPlayer(playerName);
+        if (checkNegativeBalance(p)){
+            p.makeBankrupt();
+        }
+
+    }
+
+    public boolean checkNegativeBalance(Player player){
+        return player.getMoney() < 0;
+    }
+
 }
