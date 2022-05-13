9237bea0d78665f17d73cad2bd18ca0e9d6f5a34
thibo.verbeerst <thibo.verbeerst@student.howest.be>
2022-05-13 11:35:11 +0200

    check if last turn is over

--- src/main/java/be/howest/ti/monopoly/logic/implementation/checkers/game/TurnCheck.java
+++ src/main/java/be/howest/ti/monopoly/logic/implementation/checkers/game/TurnCheck.java
@@ -1,20 +1,55 @@
 package be.howest.ti.monopoly.logic.implementation.checkers.game;
 
+import be.howest.ti.monopoly.logic.exceptions.IllegalMonopolyActionException;
 import be.howest.ti.monopoly.logic.implementation.game.Game;
 import be.howest.ti.monopoly.logic.implementation.game.Turn;
 import be.howest.ti.monopoly.logic.implementation.game.player.Player;
+
+import java.util.List;
 
 public class TurnCheck {
 
     private final Turn turn;
     private final Game game;
     private final Player player;
+    private final List<Turn> previousTurns;
+    private final Turn lastTurn;
 
     public TurnCheck(Turn turn){
         this.turn = turn;
         this.game = turn.receiveGame();
         this.player = turn.receivePlayer();
+        this.previousTurns = game.getTurns();
+        this.lastTurn = previousTurns.get(previousTurns.size() - 1);
     }
+
+    public void checkIfPlayerCanRoll(){
+        checkIfLastTurnIsOver();
+    }
+
+    private void checkIfLastTurnIsOver(){
+        if (!lastTurn.getFinished()){
+            throw new IllegalMonopolyActionException("Previous turn isn't finished yet.");
+        }
+    }
+
+    // flag in Turn that tells if previous turn is finished.
+        // Basic tile? set flag true by default.
+        // Property tile? set flag when bought (or not bought) a property
+
+    // 1. was the landed on tile of prev turn a property?
+        // has prev player bought (or not) the property he landed on?
+    // 2. check if player rolled double (prev)
+        // check if player rolled double (3 times) -> use static?
+    // 3. check if player is next in row
+
+
+    // check if player is in jail
+        // check for how long he/she has been in jail?
+        // used jail free card?
+        // paid prison fine?
+    // check if player has negative balance... Yes? Bankrupt.
+
 
 
 }
