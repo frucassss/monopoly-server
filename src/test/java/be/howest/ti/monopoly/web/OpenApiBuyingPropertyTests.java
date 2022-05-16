5104eaf825730d8c273fb82cbc3ab76406f871dc
Michiel Stragier <Michiel.stragier@gmail.com>
2022-05-16 15:45:38 +0200

    test dont buy property

--- src/test/java/be/howest/ti/monopoly/web/OpenApiBuyingPropertyTests.java
+++ src/test/java/be/howest/ti/monopoly/web/OpenApiBuyingPropertyTests.java
@@ -23,9 +23,14 @@ class OpenApiBuyingPropertyTests extends OpenApiTestsBase {
 
     @Test
     void buyPropertyUnauthorized(final VertxTestContext testContext) {
+        service.setDelegate(new ServiceAdapter(){
+            @Override
+            public void buyProperty(String gameId, String playerName, String propertyName){}
+        });
+
         post(
                 testContext,
-                "/games/game-id/players/Alice/properties/some-property",
+                "/games/group00/players/Alice/properties/some-property",
                 null,
                 response -> assertErrorResponse(response, 401)
         );
@@ -33,19 +38,28 @@ class OpenApiBuyingPropertyTests extends OpenApiTestsBase {
 
     @Test
     void dontBuyProperty(final VertxTestContext testContext) {
+        service.setDelegate(new ServiceAdapter(){
+            @Override
+            public void dontBuyProperty(String gameId, String playerName, String propertyName){}
+        });
         delete(
                 testContext,
-                "/games/game-id/players/Alice/properties/some-property",
-                "some-token",
-                response -> assertNotYetImplemented(response, "dontBuyProperty")
+                "/games/group00/players/Alice/properties/some-property",
+                "group00-Alice",
+                response -> assertOkResponse(response)
         );
     }
 
     @Test
     void dontBuyPropertyUnauthorized(final VertxTestContext testContext) {
+        service.setDelegate(new ServiceAdapter(){
+            @Override
+            public void dontBuyProperty(String gameId, String playerName, String propertyName){}
+        });
+
         delete(
                 testContext,
-                "/games/game-id/players/Alice/properties/some-property",
+                "/games/group00/players/Alice/properties/some-property",
                 null,
                 response -> assertErrorResponse(response, 401)
         );
